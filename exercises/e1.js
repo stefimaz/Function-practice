294f912e7afed8f23eb16228b8a6bc5d9e779d68
Stephane Masyn <stefimaz@gmail.com>
2024-03-20 15:52:16 -0400

    Update e1.js

--- exercises/e1.js
+++ exercises/e1.js
@@ -14,6 +14,9 @@
 */
 
 // Your code goes here
+function logMessage() {
+  console.log('Hello, World!');
+}
 
 
 
@@ -30,7 +33,9 @@
 */
 
 // Your code goes here...
-
+function logMyMessage(message) {
+  console.log(message);
+}
 
 
 
@@ -45,7 +50,9 @@
 */
 
 // Your code goes here...
-
+function logMessageWithMyName(name) {
+  console.log('My name is ' + name + '!');
+}
 
 
 
@@ -61,7 +68,9 @@
 */
 
 // Your code goes here...
-
+function logMyInfo(name, age) {
+  console.log('My name is ' + name + '. I am ' + age + ' years old!');
+}
 
 
 
